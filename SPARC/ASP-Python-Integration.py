4965df081200a9ed35a3a2a5c25195d120d7dca5
Jack Francis <jfrancis9999@hotmail.co.uk>
2019-10-07 15:47:31 +0100

    Adds function for splitting answr sets into components

--- SPARC/ASP-Python-Integration.py
+++ SPARC/ASP-Python-Integration.py
@@ -1,10 +1,26 @@
 import subprocess
 
-def get_answer_set(file):
+def getAnswerSets(file):
+#Takes ASP file as input and returns its answer set
     cmd = 'java -jar sparc.jar {} -A'.format(file)
     answer_set = subprocess.check_output(cmd)
-    print(answer_set)
+    answer_set = answer_set.decode('utf-8')
+    return answer_set
+
+
+def splitAnswerSets(set_to_split):
+#splits answer sets into individual answer sets, then creates a list of each individual instruction within each answer set
+    split_string = set_to_split.splitlines()
+    split_string = split_string[::2]
+    split_string = list(map(lambda x: x[1:-1], split_string))
+    split_string = [x.split(', ') for x in split_string]
+    return split_string
+
+def removeNotTrue(answerset):
+    
 
 
 if __name__ == "__main__":
-    get_answer_set("s_ancestors.sp")
+    #get_answer_set("s_ancestors.sp")
+    a = splitAnswerSets(getAnswerSets("s_bwplan.sp"))
+    print(a[0])
