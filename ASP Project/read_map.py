84ac3e31427494071ec8ae0071360ea9648ed2c0
Jack Francis <jfrancis9999@hotmail.co.uk>
2019-10-22 20:57:14 +0100

    Adds a non-skimage option for reading map using pillow

--- ASP Project/read_map.py
+++ ASP Project/read_map.py
@@ -1,9 +1,23 @@
 import numpy as np
 from skimage.io import imread
+from PIL import Image
 
-def map_read(map):
+def map_read_skimage(map):
     map_arr = imread(map)
     result = map_arr[:, :, 0]
+    for i_height in range(0,result.shape[0]):
+        for i_width in range(0,result.shape[1]):
+            if result[i_height][i_width] < 100:
+                result[i_height][i_width] = 0
+            elif result[i_height][i_width] >225:
+                result [i_height][i_width] = 1
+            else:
+                result[i_height][i_width] = 2
+    return result
+
+def map_read(map):
+    map_arr = np.asarray(Image.open('map.jpeg'))
+    result = np.copy(map_arr[:, :, 0])
     for i_height in range(0,result.shape[0]):
         for i_width in range(0,result.shape[1]):
             if result[i_height][i_width] < 100:
